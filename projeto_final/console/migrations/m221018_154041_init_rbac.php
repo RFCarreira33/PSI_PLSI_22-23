e905e67d5d8eb1255dcbee6154eb0357a3b028a8
Rodrigo <2213146@my.ipleiria.pt>
2022-10-20 14:19:45 +0100

    Login and Permissions

--- projeto_final/console/migrations/m221018_154041_init_rbac.php
+++ projeto_final/console/migrations/m221018_154041_init_rbac.php
@@ -30,17 +30,30 @@ class m221018_154041_init_rbac extends Migration
     {
         $auth = Yii::$app->authManager;
 
+        //criar permissoes
+        $backendLogin = $auth->createPermission('backendLogin');
+        $backendLogin->description = 'Login for Backend';
+        $auth->add($backendLogin);
+
+        $frontendLogin = $auth->createPermission('frontendLogin');
+        $frontendLogin->description = 'Login for Frontend';
+        $frontendLogin->add($frontendLogin);
+
+
         //criar role funcionario
         $funcionario = $auth->createRole('funcionario');
         $auth->add($funcionario);
+        $auth->addChild($funcionario, $backendLogin);
 
         //criar role cliente
         $cliente = $auth->createRole('cliente');
         $auth->add($cliente);
+        $auth->addChild($cliente, $frontendLogin);
 
         //criar role admin e dar asign do user id 1
         $admin = $auth->createRole('admin');
         $auth->add($admin);
+        $auth->addChild($admin, $funcionario);
         $auth->assign($admin, 1);
     }
 
--- projeto_final/frontend/controllers/SiteController.php
+++ projeto_final/frontend/controllers/SiteController.php
@@ -88,7 +88,7 @@ class SiteController extends Controller
         }
 
         $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+        if ($model->load(Yii::$app->request->post()) && $model->login() && Yii::$app->user->can('frontendLogin')) {
             return $this->goBack();
         }
 
