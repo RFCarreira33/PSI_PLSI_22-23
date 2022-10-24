112233f7e8a12d842d3269ffb4b3ad2a6f956285
JoaoJesus1337 <92525033+JoaoJesus1337@users.noreply.github.com>
2022-10-24 21:04:27 +0100

    backend index widgets fix

--- projeto_final/backend/views/site/index.php
+++ projeto_final/backend/views/site/index.php
@@ -5,198 +5,118 @@ use yii\helpers\Url;
 $this->title = 'Starter Page';
 $this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
-<<<<<<< Updated upstream
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-6">
-            <?= \hail812\adminlte\widgets\Alert::widget([
-                'type' => 'success',
-                'body' => '<h3>Congratulations!</h3>',
-            ]) ?>
-            <?= \hail812\adminlte\widgets\Callout::widget([
-                'type' => 'danger',
-                'head' => 'I am a danger callout!',
-                'body' => 'There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.'
-            ]) ?>
-        </div>
-=======
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-<<<<<<< Updated upstream
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
->>>>>>> Stashed changes
+<div class="col-lg-6">
+    <?= \hail812\adminlte\widgets\Alert::widget([
+        'type' => 'success',
+        'body' => '<h3>Congratulations!</h3>',
+    ]) ?>
+    <?= \hail812\adminlte\widgets\Callout::widget([
+        'type' => 'danger',
+        'head' => 'I am a danger callout!',
+        'body' => 'There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.'
+    ]) ?>
+</div>
+<div class="col-md-4 col-sm-6 col-12">
+    <?= \hail812\adminlte\widgets\InfoBox::widget([
+        'text' => 'Uploads',
+        'number' => '13,648',
+        'theme' => 'gradient-warning',
+        'icon' => 'far fa-copy',
+    ]) ?>
+</div>
+<div class="row">
+    <div class="col-12 col-sm-6 col-md-3">
+        <?= \hail812\adminlte\widgets\InfoBox::widget([
+            'text' => 'CPU Traffic',
+            'number' => '10 <small>%</small>',
+            'icon' => 'fas fa-cog',
+        ]) ?>
     </div>
 
-    <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'CPU Traffic',
-                'number' => '10 <small>%</small>',
-                'icon' => 'fas fa-cog',
-            ]) ?>
-        </div>
+    <div class="col-md-4 col-sm-6 col-12">
+        <?= \hail812\adminlte\widgets\InfoBox::widget([
+            'text' => 'Messages',
+            'number' => '1,410',
+            'icon' => 'far fa-envelope',
+        ]) ?>
     </div>
-
-    <div class="row">
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Messages',
-                'number' => '1,410',
-                'icon' => 'far fa-envelope',
-            ]) ?>
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Bookmarks',
-                'number' => '410',
-                'theme' => 'success',
-                'icon' => 'far fa-flag',
-            ]) ?>
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Uploads',
-                'number' => '13,648',
-                'theme' => 'gradient-warning',
-                'icon' => 'far fa-copy',
-            ]) ?>
-        </div>
-    </div>
-
-<<<<<<< Updated upstream
-=======
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-=======
->>>>>>> Stashed changes
-    <div class="row">
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Bookmarks',
-                'number' => '41,410',
-                'icon' => 'far fa-bookmark',
-                'progress' => [
-                    'width' => '70%',
-                    'description' => '70% Increase in 30 Days'
-                ]
-            ]) ?>
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <?php $infoBox = \hail812\adminlte\widgets\InfoBox::begin([
-                'text' => 'Likes',
-                'number' => '41,410',
-                'theme' => 'success',
-                'icon' => 'far fa-thumbs-up',
-                'progress' => [
-                    'width' => '70%',
-                    'description' => '70% Increase in 30 Days'
-                ]
-            ]) ?>
-            <?= \hail812\adminlte\widgets\Ribbon::widget([
-                'id' => $infoBox->id . '-ribbon',
-                'text' => 'Ribbon',
-            ]) ?>
-            <?php \hail812\adminlte\widgets\InfoBox::end() ?>
-<<<<<<< Updated upstream
-=======
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Events',
-                'number' => '41,410',
-                'theme' => 'gradient-warning',
-                'icon' => 'far fa-calendar-alt',
-                'progress' => [
-                    'width' => '70%',
-                    'description' => '70% Increase in 30 Days'
-                ],
-            ]) ?>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => $nClientes,
-                'text' => 'Clientes Registados',
-                'icon' => 'fas fa-user-plus',
-                'linkText' => 'Ver Clientes',
-                'linkUrl' => Url::toRoute(["cliente/index"])
-            ]) ?>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => $nFaturas,
-                'text' => 'Numero de Faturas',
-                'icon' => 'far fa-copy',
-                'linkText' => 'Ver Faturas',
-                'linkUrl' => Url::toRoute(["fatura/index"])
-            ]) ?>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => $somaFatura,
-                'text' => 'Total Faturado',
-                'icon' => 'far fa-copy',
-                'linkText' => 'Ver Faturas',
-                'linkUrl' => Url::toRoute(["fatura/index"])
-            ]) ?>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Events',
-                'number' => '41,410',
-                'theme' => 'gradient-warning',
-                'icon' => 'far fa-calendar-alt',
-                'progress' => [
-                    'width' => '70%',
-                    'description' => '70% Increase in 30 Days'
-                ],
-                'loadingStyle' => true
-            ]) ?>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => $nClientes,
-                'text' => 'Clientes Registados',
-                'icon' => 'fas fa-user-plus',
-                'linkText' => 'Ver Clientes',
-                'linkUrl' => Url::toRoute(["cliente/index"])
-            ]) ?>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?php $smallBox = \hail812\adminlte\widgets\SmallBox::begin([
-                'title' => '150',
-                'text' => 'New Orders',
-                'icon' => 'fas fa-shopping-cart',
-                'theme' => 'success'
-            ]) ?>
-            <?= \hail812\adminlte\widgets\Ribbon::widget([
-                'id' => $smallBox->id . '-ribbon',
-                'text' => 'Ribbon',
-                'theme' => 'warning',
-                'size' => 'lg',
-                'textSize' => 'lg'
-            ]) ?>
-            <?php \hail812\adminlte\widgets\SmallBox::end() ?>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => '44',
-                'text' => 'User Registrations',
-                'icon' => 'fas fa-user-plus',
-                'theme' => 'gradient-success',
-            ]) ?>
-        </div>
+    <div class="col-md-4 col-sm-6 col-12">
+        <?= \hail812\adminlte\widgets\InfoBox::widget([
+            'text' => 'Bookmarks',
+            'number' => '410',
+            'theme' => 'success',
+            'icon' => 'far fa-flag',
+        ]) ?>
     </div>
 </div>
+<div class="row">
+    <div class="col-md-4 col-sm-6 col-12">
+        <?= \hail812\adminlte\widgets\InfoBox::widget([
+            'text' => 'Events',
+            'number' => '41,410',
+            'theme' => 'gradient-warning',
+            'icon' => 'far fa-calendar-alt',
+            'progress' => [
+                'width' => '70%',
+                'description' => '70% Increase in 30 Days'
+            ],
+        ]) ?>
+    </div>
+    <div class="col-md-4 col-sm-6 col-12">
+        <?= \hail812\adminlte\widgets\InfoBox::widget([
+            'text' => 'Bookmarks',
+            'number' => '41,410',
+            'icon' => 'far fa-bookmark',
+            'progress' => [
+                'width' => '70%',
+                'description' => '70% Increase in 30 Days'
+            ]
+        ]) ?>
+    </div>
+
+    <div class="col-md-4 col-sm-6 col-12">
+        <?php $infoBox = \hail812\adminlte\widgets\InfoBox::begin([
+            'text' => 'Likes',
+            'number' => '41,410',
+            'theme' => 'success',
+            'icon' => 'far fa-thumbs-up',
+            'progress' => [
+                'width' => '70%',
+                'description' => '70% Increase in 30 Days'
+            ]
+        ]) ?>
+        <?= \hail812\adminlte\widgets\Ribbon::widget([
+            'id' => $infoBox->id . '-ribbon',
+            'text' => 'Ribbon',
+        ]) ?>
+        <?php \hail812\adminlte\widgets\InfoBox::end() ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+        <?= \hail812\adminlte\widgets\SmallBox::widget([
+            'title' => $nClientes,
+            'text' => 'Clientes Registados',
+            'icon' => 'fas fa-user-plus',
+            'linkText' => 'Ver Clientes',
+            'linkUrl' => Url::toRoute(["cliente/index"])
+        ]) ?>
+    </div>
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+        <?= \hail812\adminlte\widgets\SmallBox::widget([
+            'title' => $nFaturas,
+            'text' => 'Numero de Faturas',
+            'icon' => 'far fa-copy',
+            'linkText' => 'Ver Faturas',
+            'linkUrl' => Url::toRoute(["fatura/index"])
+        ]) ?>
+    </div>
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+        <?= \hail812\adminlte\widgets\SmallBox::widget([
+            'title' => $somaFatura,
+            'text' => 'Total Faturado',
+            'icon' => 'far fa-copy',
+            'linkText' => 'Ver Faturas',
+            'linkUrl' => Url::toRoute(["fatura/index"])
+        ]) ?>
+    </div>
