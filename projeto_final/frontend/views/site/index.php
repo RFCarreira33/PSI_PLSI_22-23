11743e2efb2bccac5869fe1088aec517a4283de7
JPTRON <joaopedroferreira0318@gmail.com>
2022-10-29 01:37:54 +0100

    Pagination

--- projeto_final/frontend/views/site/index.php
+++ projeto_final/frontend/views/site/index.php
@@ -4,6 +4,7 @@
 
 use yii\helpers\Url;
 use yii\bootstrap5\Html;
+use yii\widgets\LinkPager;
 
 $this->title = 'My Yii Application';
 ?>
@@ -19,30 +20,38 @@ $this->title = 'My Yii Application';
     <div class="container px-4 px-lg-5 mt-5">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             <?php foreach ($produtos as $produto) { ?>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <a style="text-decoration: none;color:black;" href="<?= Url::toRoute(["produto/view", "id" => $produto->id]) ?>">
-                            <img class="card-img-top" src="img/<?php echo $produto->imagem ?>" alt="..." />
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <h5 class="fw-bolder"><?php echo $produto->nome ?></h5>
-                        </a>
-                        <!-- Product price-->
-                        <?php echo $produto->preco ?>€
+            <div class="col mb-5">
+                <div class="card h-100">
+                    <!-- Product image-->
+                    <a style="text-decoration: none;color:black;"
+                        href="<?= Url::toRoute(["produto/view", "id" => $produto->id]) ?>">
+                        <img class="card-img-top" src="img/<?php echo $produto->imagem ?>" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder"><?php echo $produto->nome ?></h5>
+                    </a>
+                    <!-- Product price-->
+                    <?php echo $produto->preco ?>€
 
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div style="padding-top:10px" class="text-center"><a class="btn btn-outline-dark mt-auto" href="<?= Url::toRoute(["produto/view", "id" => $produto->id]) ?>">
-                                    Adicionar<i class="bi-cart-fill me-1"></i>
-                                </a></div>
-                        </div>
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div style="padding-top:10px" class="text-center"><a class="btn btn-outline-dark mt-auto"
+                                href="<?= Url::toRoute(["produto/view", "id" => $produto->id]) ?>">
+                                Adicionar<i class="bi-cart-fill me-1"></i>
+                            </a></div>
                     </div>
                 </div>
+            </div>
         </div>
     </div>
-<?php } ?>
-</div>
-</div>
+    <?php } ?>
+    </div>
+    </div>
 </section>
+
+<?php
+    echo LinkPager::widget([
+        'pagination' => $pages,
+    ]);
+?>
