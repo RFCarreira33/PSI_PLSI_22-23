d34b75afcc102658b29fcf8dddbdac00b8bc22c3
Rodrigoporextenso <2213146@my.ipleiria.pt>
2022-10-30 15:56:15 +0000

    updated CarrinhoCreate

--- projeto_final/frontend/controllers/CarrinhoController.php
+++ projeto_final/frontend/controllers/CarrinhoController.php
@@ -4,11 +4,13 @@ namespace frontend\controllers;
 
 use common\models\Produto;
 use common\models\Carrinho;
+use common\models\Dados;
 use frontend\models\CarrinhoSearch;
 use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * CarrinhoController implements the CRUD actions for Carrinho model.
@@ -23,6 +25,15 @@ class CarrinhoController extends Controller
         return array_merge(
             parent::behaviors(),
             [
+                'access' => [
+                    'class' => AccessControl::class,
+                    'rules' => [
+                        [
+                            'allow' => true,
+                            'roles' => ['cliente']
+                        ],
+                    ],
+                ],
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
@@ -63,31 +74,24 @@ class CarrinhoController extends Controller
      */
     public function actionCreate($id)
     {
-        $model = new Carrinho();
-        $carrinhos = Carrinho::find()->all();
-
-        $model->idCliente = Yii::$app->user->id;
-        $model->idProduto = $id;
-        $model->Quantidade = 2;
-        
-        foreach($carrinhos as $carrinho)
-        {
-            if($carrinho->idProduto == $id)
-            {
-                $carrinho = Carrinho::find()->where(['idProduto' => $id])->one();
-                $carrinho->Quantidade = $carrinho->Quantidade + 1;
-                $carrinho -> save();
-                $carrinhos = Carrinho::find()->where(['idCliente' => Yii::$app->user->id])->all();
-                return $this->render('view', ['carrinhos' => $carrinhos]);
-            }
-        }
-        
-        if ($model->save()) 
-        {
-            return $this->render('view', ['carrinhos' => $carrinhos]);
+        if (!Yii::$app->user->can('carrinho')) {
+            return $this->redirect('site/login');
         }
 
-        return $this->redirect('site/index');
+        $dados = Dados::find()->where(['idUser' => Yii::$app->user->id])->one();
+        $carrinho = $dados->getCarrinhos()->where(['idProduto' => $id])->one();
+
+        if ($carrinho == null) {
+            $carrinho = new Carrinho;
+            $carrinho->idCliente = $dados->idUser;
+            $carrinho->idProduto = $id;
+            $carrinho->quantidade + 31; //passar quantidades não a toa 
+        } else {
+            $carrinho->quantidade += 22;
+        }
+        $carrinho->save();
+        $carrinhos = $dados->getCarrinhos()->all();
+        return $this->render('view', ['carrinhos' => $carrinhos]);
     }
 
     /**
