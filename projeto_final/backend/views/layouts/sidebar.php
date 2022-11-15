a16e4c1635baf6b0c6e631287f560a432fc79b8a
JoaoJesus1337 <92525033+JoaoJesus1337@users.noreply.github.com>
2022-11-15 09:55:23 +0000

    backend username update

--- projeto_final/backend/views/layouts/sidebar.php
+++ projeto_final/backend/views/layouts/sidebar.php
@@ -1,25 +1,25 @@
 <?php
 
 use yii\helpers\Url;
+use common\models\User;
 ?>
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= Url::toRoute('site/index') ?>" class="brand-link">
-        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-            style="opacity: .8">
+        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
+    <div1bar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?= $assetDir ?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="/img/1.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <?php $user = User::find(Yii::$app->user->id)->one() ?>
+                <a href="#" class="d-block"><?php echo $user->username ?></a>
             </div>
         </div>
 
@@ -81,6 +81,6 @@ use yii\helpers\Url;
             ?>
         </nav>
         <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
+        </div>
+        <!-- /.sidebar -->
 </aside>
