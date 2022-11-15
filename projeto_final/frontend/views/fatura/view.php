a0aae86ae2c4f9b05a9a7ff34164b233bbb6cdfd
JoaoJesus1337 <92525033+JoaoJesus1337@users.noreply.github.com>
2022-11-15 00:20:25 +0000

    fatura view update

--- projeto_final/frontend/views/fatura/view.php
+++ projeto_final/frontend/views/fatura/view.php
@@ -5,42 +5,47 @@ use yii\widgets\DetailView;
 
 /** @var yii\web\View $this */
 /** @var common\models\Fatura $model */
-
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Faturas', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
-\yii\web\YiiAsset::register($this);
 ?>
-<div class="fatura-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'id_Cliente',
-            'nome',
-            'nif',
-            'codPostal',
-            'telefone',
-            'morada',
-            'email:email',
-            'dataFatura',
-            'valorTotal',
-            'valorIva',
-        ],
-    ]) ?>
-
+<hr>
+<div>
+    <div>
+        <h5>Cliente</h5>
+        <br>
+        <p>Nome: <?= $model->nome ?></p>
+        <p>NIF: <?= $model->nif ?></p>
+        <p>Telefone: <?= $model->telefone ?></p>
+        <p><?= $model->morada ?> <?= $model->codPostal ?></p>
+        <p>Email: <?= $model->email ?></p>
+    </div>
 </div>
+<div>
+    <table class="table">
+        <thead>
+            <th>Referencia</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Uni.</th>
+            <th>IVA %</th>
+            <th>IVA</th>
+            <th>Subtotal</th>
+        </thead>
+        <tbody>
+            <?php foreach ($model->linhafaturas as $linha) { ?>
+                <tr>
+                    <td><?= $linha->produto->referencia ?></td>
+                    <td><?= $linha->produto->descricao ?></td>
+                    <td><?= $linha->quantidade ?></td>
+                    <td><?= $linha->produto->preco ?>€</td>
+                    <td><?= $linha->produto->iva->percentagem ?>%</td>
+                    <td><?= $linha->valorIva ?>€</td>
+                    <td><?= $linha->valor ?>€</td>
+                </tr>
+            <?php } ?>
+        </tbody>
+        <tfooter>
+            <td><b>Total</b></td>
+            <td><?= $model->valorTotal ?>€</td>
+        </tfooter>
+    </table>
+</div>
+<hr>
