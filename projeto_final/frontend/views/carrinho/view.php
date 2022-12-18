464b585c388e8f3326946be1191131a345511b00
JPTRON <joaopedroferreira0318@gmail.com>
2022-12-18 03:34:00 +0000

    switch quantity error from alerts to notifications

--- projeto_final/frontend/views/carrinho/view.php
+++ projeto_final/frontend/views/carrinho/view.php
@@ -6,6 +6,7 @@ use yii\helpers\Url;
 ?>
 
 <head>
+    <link href="/css/cart.css" rel="stylesheet" />
     <style>
     @media (min-width: 1025px) {
         .h-custom {
@@ -102,21 +103,28 @@ use yii\helpers\Url;
                                                 <?= $carrinho->produto->preco *  $carrinho->Quantidade ?>€</h6>
                                         </div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
                                         <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a data-method="POST" style='text-decoration:none '
-                                                href="<?= Url::toRoute(["carrinho/delete", 'id_Produto' => $carrinho->id_Produto]) ?>">
-                                                X
+                                            <a data-method="POST" class="pointer cross" style='text-decoration:none '
+                                                onclick="remove('Pretende remover este produto do carrinho?', '<?= Url::toRoute(['carrinho/delete', 'id_Produto' => $carrinho->id_Produto], true)?>')">
+                                                <svg width="1em" height="1em" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.59 0L5 3.59 1.41 0 0 1.41 3.59 5 0 8.59 1.41 10 5 6.41 8.59 10 10 8.59 6.41 5 10 1.41 8.59 0z" fill="currentColor"></path></svg>
                                             </a>
                                         </div>
                                     </div>
                                     <?php } ?>
                                     <hr class="my-4">
+                                    <?php
+                                        if(sizeof($carrinhos) > 0)
+                                        { ?>
+                                            <div class="pt-5">
+                                                <h6 class="mb-0"><a data-method="POST" class="text-body pointer" onclick="remove('Pretende limpar o carrinho?', '<?= Url::toRoute('carrinho/clear', true) ?>')">Limpar Carrinho</a></h6>
+                                            </div>
+                                        <?php
+                                        }
+                                    ?>
                                     <div class="pt-5">
-                                        <h6 class="mb-0"><a data-method="POST" class="text-body"
-                                                href="<?= Url::toRoute("carrinho/clear") ?>">Limpar Carrinho</a></h6>
-                                    </div>
-                                    <div class="pt-5">
-                                        <h6 class="mb-0"><a href="<?= Url::home() ?>" class="text-body">Voltar a
-                                                loja</a></h6>
+                                        <h6 class="mb-0"><a href="<?= Url::home() ?>" class="text-body">
+                                        <svg width="1em" height="1em" viewBox="0 0 19 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.249 3.625l2.323-2.266L5.302.125.801 4.5l4.501 4.375 1.27-1.234-2.323-2.266h14.558v-1.75H4.249z" fill="currentColor"></path></svg>
+                                        Voltar à loja
+                                        </a></h6>
                                     </div>
                                 </div>
                             </div>
@@ -169,17 +177,17 @@ use yii\helpers\Url;
 </section>
 
 <script>
+    var timer;
+    
     function changeQuantity(el, value)
     {
         var input = el.parentNode.querySelector("input[name='quantityInput']");
         var newValue = parseInt(input.value) + value;
-
-        if(newValue > 20) { newValue = 20; alert("Não foi possível adicionar o produto. O máximo disponível é 20.") }
-        if(newValue < 1) { newValue = 1; alert("Introduza uma quantidade superior a zero.") }
+        if(newValue > 20) { newValue = 20;}
+        if(newValue < 1)  { newValue = 1; }
         
+        callAjax(newValue, input.getAttribute("data-product"));
         input.value = newValue;
-
-        callAjax(input.value, input.getAttribute("data-product"), );
     }
 
     var quantityInputs = document.getElementsByName("quantityInput");
@@ -188,10 +196,22 @@ use yii\helpers\Url;
     {
         input.addEventListener('input', function(e)
         {
-            if(e.srcElement.value > 20) { e.srcElement.value = 20; alert("Não foi possível adicionar o produto. O máximo disponível é 20.") }
-            if(e.srcElement.value < 1) { e.srcElement.value = 1; alert("Introduza uma quantidade superior a zero.") }
-
-            callAjax(e.srcElement.value, input.getAttribute("data-product"));
+            clearTimeout(timer);
+            timer = setTimeout(function()
+            {
+                if(e.srcElement.value > 20) 
+                { 
+                    var notification = alertify.notify("Não foi possível adicionar o produto. O máximo disponível é 20.", 'error', 2); 
+                }
+                else if(e.srcElement.value < 1) 
+                { 
+                    var notification = alertify.notify("Introduza uma quantidade superior a zero.", 'error', 2); 
+                }
+                else
+                {
+                    callAjax(e.srcElement.value, input.getAttribute("data-product"));
+                }
+            }.bind(this), 500);
         })
     })
 
@@ -227,5 +247,14 @@ use yii\helpers\Url;
                 $("#totalPrice").text(result.totalPrice + "€");
             }
         });
+    }
+
+    function remove(message, route)
+    {
+        alertify.confirm("", message,
+            function()
+            {
+                window.location = route;
+            }, function(){});
     }
 </script>
